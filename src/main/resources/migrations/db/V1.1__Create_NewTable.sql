8e767489c2e67ccbef0c4ddcea0ff92eb8e22b04
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-08 23:02:59 +0530

    Update and rename V1_1_add_data.sql to V1.1__Create_NewTable.sql

---
+++ src/main/resources/migrations/db/V1.1__Create_NewTable.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE  products (
+         productID    INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+         productCode  CHAR(3)       NOT NULL DEFAULT '',
+         name         VARCHAR(30)   NOT NULL DEFAULT '',
+         quantity     INT UNSIGNED  NOT NULL DEFAULT 0,
+         price        DECIMAL(7,2)  NOT NULL DEFAULT 99999.99,
+         PRIMARY KEY  (productID)
+       );
