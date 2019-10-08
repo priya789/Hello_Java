6c1414973083d9c3f43c85a372678ae5755e3f47
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-09 01:05:33 +0530

    Create V1.2__Insertvalues.sql

---
+++ src/main/resources/migrations/db/V1.2__Insertvalues.sql
@@ -0,0 +1,6 @@
+INSERT INTO DataFly (productID) VALUES (1);
+INSERT INTO DataFly (productCode) VALUES (001);
+INSERT INTO DataFly (name) VALUES (tina);
+INSERT INTO DataFly (name) VALUES (mona);
+INSERT INTO DataFly (quantity) VALUES (1);
+INSERT INTO DataFly (price) VALUES (2000);
