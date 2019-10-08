412e7666b7506ff682cba1adab4ce703b02eeab2
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-09 01:09:39 +0530

    Update V1.2__Insertvalues.sql

--- src/main/resources/migrations/db/V1.2__Insertvalues.sql
+++ src/main/resources/migrations/db/V1.2__Insertvalues.sql
@@ -1,6 +1,1 @@
 INSERT INTO products (productID) VALUES (1);
-INSERT INTO products (productCode) VALUES (001);
-INSERT INTO products (name) VALUES (tina);
-INSERT INTO products (name) VALUES (mona);
-INSERT INTO products (quantity) VALUES (1);
-INSERT INTO products (price) VALUES (2000);
