4ddea268e83e5225ef0af3aa2d7ae311585572d2
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-09 01:08:10 +0530

    Update V1.2__Insertvalues.sql

--- src/main/resources/migrations/db/V1.2__Insertvalues.sql
+++ src/main/resources/migrations/db/V1.2__Insertvalues.sql
@@ -1,6 +1,6 @@
-INSERT INTO DataFly (productID) VALUES (1);
-INSERT INTO DataFly (productCode) VALUES (001);
-INSERT INTO DataFly (name) VALUES (tina);
-INSERT INTO DataFly (name) VALUES (mona);
-INSERT INTO DataFly (quantity) VALUES (1);
-INSERT INTO DataFly (price) VALUES (2000);
+INSERT INTO products (productID) VALUES (1);
+INSERT INTO products (productCode) VALUES (001);
+INSERT INTO products (name) VALUES (tina);
+INSERT INTO products (name) VALUES (mona);
+INSERT INTO products (quantity) VALUES (1);
+INSERT INTO products (price) VALUES (2000);
