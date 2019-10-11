791155238f96a237c8e018f091e25c901af49b31
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-11 15:40:16 +0530

    Delete V1__Create_NewTable.sql

--- src/main/resources/migrations/db/V1__Create_NewTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE  products (
-         productID    INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-         productCode  CHAR(3)       NOT NULL DEFAULT '',
-         name         VARCHAR(30)   NOT NULL DEFAULT '',
-         quantity     INT UNSIGNED  NOT NULL DEFAULT 0,
-         price        DECIMAL(7,2)  NOT NULL DEFAULT 99999.99,
-         PRIMARY KEY  (productID)
-       );
