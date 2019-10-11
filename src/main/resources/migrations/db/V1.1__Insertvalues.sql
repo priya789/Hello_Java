cbdb6a19d096c093ef16c959f9112f3d618532b9
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-11 15:41:38 +0530

    Update V1.1__Insertvalues.sql

--- src/main/resources/migrations/db/V1.1__Insertvalues.sql
+++ src/main/resources/migrations/db/V1.1__Insertvalues.sql
@@ -1,4 +1,4 @@
-INSERT INTO products (productID) VALUES (1);
+INSERT INTO products (productID) VALUES (3);
 INSERT INTO products (productCode) VALUES (A);
 INSERT INTO products (name) VALUES (tina);
 INSERT INTO products (quantity) VALUES (1);
