f6c7d3d8dc46b9c82cd809445db0d3d1ae4361e2
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-11 15:31:21 +0530

    Update and rename V2__Insertvalues.sql to V1__Insertvalues.sql

--- src/main/resources/migrations/db/V1__Insertvalues.sql
+++ src/main/resources/migrations/db/V1__Insertvalues.sql
@@ -2,4 +2,4 @@ INSERT INTO products (productID) VALUES (1);
 INSERT INTO products (productCode) VALUES (A);
 INSERT INTO products (name) VALUES (tina);
 INSERT INTO products (quantity) VALUES (1);
-INSERT INTO products (price) VALUES (2000.00);
+INSERT INTO products (price) VALUES (1000.00);
