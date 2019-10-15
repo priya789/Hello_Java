97fc5854db4c1969ed69647c701608d8842e7d86
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-15 13:13:52 +0530

    Delete V1.1__Insertvalues.sql

--- src/main/resources/migrations/db/V1.1__Insertvalues.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO products (productID) VALUES (3);
-INSERT INTO products (productCode) VALUES (A);
-INSERT INTO products (name) VALUES (tina);
-INSERT INTO products (quantity) VALUES (1);
-INSERT INTO products (price) VALUES (1000.00);
