563837100efcd00e056efe703255a6b6d89af264
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 23:41:48 +0530

    Update V3_1__MultipleProducts.sql

--- src/main/resources/migrations/db/V3_1__MultipleProducts.sql
+++ src/main/resources/migrations/db/V3_1__MultipleProducts.sql
@@ -1,3 +1,3 @@
-INSERT INTO items (productID,productCode,name,quantity,price) VALUES (1,'B00','Adam',1,12.5);
-INSERT INTO items (productID,productCode,name,quantity,price) VALUES (2,'B00','Prince',1,12.5);
-INSERT INTO items (productID,productCode,name,quantity,price) VALUES (3,'B00','Danny',1,12.5);
+INSERT INTO items (productID,productCode,name,quantity,price) VALUES (2,'B00','Adam',1,12.5);
+INSERT INTO items (productID,productCode,name,quantity,price) VALUES (3,'B00','Prince',1,12.5);
+INSERT INTO items (productID,productCode,name,quantity,price) VALUES (4,'B00','Danny',1,12.5);
