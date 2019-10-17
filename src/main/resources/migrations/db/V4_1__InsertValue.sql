562aec8be96925310f4441719b7f76a7ececc264
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 22:19:03 +0530

    Update V4_1__InsertValue.sql

--- src/main/resources/migrations/db/V4_1__InsertValue.sql
+++ src/main/resources/migrations/db/V4_1__InsertValue.sql
@@ -1,2 +1,2 @@
-INSERT INTO items (productCode,name,quantity,price,productType,Availabilty) VALUES ('B00','Tide',1,12.5,'TestPrdTypes','Yes');
+INSERT INTO items (productCode,name,quantity,price,productType,Availability) VALUES ('B00','Tide',1,12.5,'TestPrdTypes','Yes');
 kjkl
