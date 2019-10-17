af9aff56f693c86488b04b50a2e2fd9dc36ff1d1
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 22:27:44 +0530

    Update and rename V4_1__InsertValue.sql to V4___InsertValue.sql

--- src/main/resources/migrations/db/V4_1__InsertValue.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO items (productCode,name,quantity,price,productType,Availability) VALUES ('B00','Tide',1,12.5,'TestPrdTypes','Yes');
---
+++ src/main/resources/migrations/db/V4___InsertValue.sql
@@ -0,0 +1,1 @@
+INSERT INTO items (productCode,name,quantity,price,productType) VALUES ('600','Surf',5,22.5,'PrdTypes');
