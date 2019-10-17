ddd3f0ff6d49cdac43534d512438a360c22e78ea
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 23:39:45 +0530

    Update V3_3__InsertProductType.sql

--- src/main/resources/migrations/db/V3_3__InsertProductType.sql
+++ src/main/resources/migrations/db/V3_3__InsertProductType.sql
@@ -1,1 +1,1 @@
-INSERT INTO items (productCode,name,quantity,price,productType) VALUES ('B00','Tide',1,12.5,'TestPrdTypes');
+INSERT INTO items (productID,productCode,name,quantity,price,productType) VALUES (4,'B00','Tide',1,12.5,'TestPrdTypes');
