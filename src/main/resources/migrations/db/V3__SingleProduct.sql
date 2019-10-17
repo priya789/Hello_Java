6257bdaa966a1a1384159c7be2135d44ade8ca81
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 23:41:20 +0530

    Update V3__SingleProduct.sql

--- src/main/resources/migrations/db/V3__SingleProduct.sql
+++ src/main/resources/migrations/db/V3__SingleProduct.sql
@@ -1,1 +1,1 @@
-INSERT INTO items (productID,productCode,name,quantity,price) VALUES (4,'A00','close up',1,12.5)
+INSERT INTO items (productID,productCode,name,quantity,price) VALUES (1,'A00','close up',1,12.5)
