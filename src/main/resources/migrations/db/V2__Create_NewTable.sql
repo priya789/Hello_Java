0bbc450823662b258fcb25839c15470e40e9aef4
priya789 <45468200+priya789@users.noreply.github.com>
2019-10-17 23:37:31 +0530

    Update V2__Create_NewTable.sql

--- src/main/resources/migrations/db/V2__Create_NewTable.sql
+++ src/main/resources/migrations/db/V2__Create_NewTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE  items (
-         productID    INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+         productID    INT UNSIGNED  NOT NULL DEFAULT,
          productCode  CHAR(3)       NOT NULL DEFAULT '',
          name         VARCHAR(30)   NOT NULL DEFAULT '',
          quantity     INT UNSIGNED  NOT NULL DEFAULT 0,
